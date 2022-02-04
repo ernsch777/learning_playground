a75f0b6bd35e95ec1c0c2a0a0d6daf5c82b702c1
Ernie Schulz <82405659+ernsch777@users.noreply.github.com>
2022-02-03 17:58:54 -0800

    Add files via upload

--- promises.js
+++ promises.js
@@ -1,14 +1,36 @@
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 2;
-    if (a == 2) {
-        resolve('Success')
-    } else {
-        reject('Failed')
-    }
-})
+const makeRequest = (location) => {
+    return new Promise((resolve, reject) => {
+        if (location === 'google') {
+            resolve('google says hi')
+        } else {
+            reject('We can only talk to google')
+        }
+    })
+}
+const processRequest = (response) => {
+    return new Promise((resolve, reject) => {
+        console.log('Processing response')
+        resolve(`extra information + ${response}`)
+    })
+}
 
-p.then((message) => {
-    console.log('This is in the then ' + message)
-}).catch((message) => {
-    console.log('This is in the catch ' + message)
-})
+// makeRequest('google').then(response => {
+//     console.log('response has been received')
+//     return processRequest(response)
+// }).then(proceessedResponse => {
+//     console.log(proceessedResponse)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+async function doWork() {
+    try {
+        const response = await makeRequest('google')
+        console.log('response received')
+        const processedResponse = await processRequest(response)
+        console.log(processedResponse)
+    } catch (err) {
+        console.log(err)
+    }
+}
+doWork()
