a24aec4d1d41e66e210fba3a737ba1f982f35557
Ernie Schulz <82405659+ernsch777@users.noreply.github.com>
2022-02-14 21:08:54 -0800

    Add files via upload

--- multiplesof35.js
+++ multiplesof35.js
@@ -1,20 +1,5 @@
-let max = 1000;
-let mulArray = [0];
-let sum = 0;
-for (let i = 1; i < max; i++) {
-    if (i * 3 >= max) {
-        break
-    }
-    mulArray.push(i * 3);
-}
-for (let i = 1; i < max; i++) {
-    if (i * 5 >= max) {
-        break
-    }
-    mulArray.push(i * 5);;
-}
-let uniqueArray = [...new Set(mulArray)];
-for (let i = 0; i < uniqueArray.length; i++) {
-    sum += uniqueArray[i];
-}
-console.log(sum);
+
+document.getElementById("submit").addEventListener('click', function () {
+    console.log("nameInput");
+    console.log("clicked");
+})
